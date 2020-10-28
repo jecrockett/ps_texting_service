7b0b4dd8ce2f0cf5acf97d97aa8df77d6cc8f050
James Crockett <jecrockett@gmail.com>
2020-10-28 13:12:04 -0400

    Update env variable name for ngrok domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << ENV['NGROK_HOST_URL']
+  config.hosts << ENV['NGROK_DOMAIN']
 end
